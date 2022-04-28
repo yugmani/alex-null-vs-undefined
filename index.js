49745b0e0c5e00363e38c49c61f947606d77d359
yugmani <yugmani@yahoo.com>
2022-04-27 18:33:58 -0700

    undefined with non existing array elements and non existing object properties

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Import stylesheets
 import './style.css';
 
+// -------------------------------------
+// ************ N U L L ****************
+// -------------------------------------
+
 //Testing for loose equality
 console.log(null == undefined); //true
 
@@ -33,3 +37,41 @@ console.log(null === false); //false
 console.log(null === NaN); //false
 
 console.log(null === ''); //false
+
+// ----------------------------------------
+// ************ U N D E F I N E D *********
+// ----------------------------------------
+
+//Declare a variable without assigning it a value
+let fruits;
+
+//Log the value of "fruits";
+console.log(fruits); //undefined
+
+//Declaring a varible as undefined
+let name = undefined;
+console.log(name); //undefined
+
+//non-existing array elements,
+let people = ['Jack', 'Aaron'];
+console.log(people[0]); //Jack
+console.log(people[2]); //undefined
+
+let myArray = [];
+console.log(myArray[0]); //undefined
+
+//non-existing object properties
+const book = {
+  title: 'Zero to One',
+  author: ['Peter Thiel', 'Blake Masters'],
+  publicationDate: 'Sep 18, 2014',
+};
+
+//accessing existing prroperties
+console.log(book.title); //Zero to One
+
+//accessing non-existing properties
+console.log(book.genre); //undefined
+
+let myObj = {};
+console.log(myObj.prop); //undefined
